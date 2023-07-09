928341deef9805c20abadbccc373332f7b2dedd0
lzzzzl <597023590@qq.com>
2023-07-10 07:53:45 +0800

    search insert position

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## 数组
 
 1. [二分查找](https://leetcode.cn/problems/binary-search/)
+2. [搜索插入位置](https://leetcode.cn/problems/search-insert-position/)
---
+++ array/2-search-insert-position/search-insert-position.py
@@ -0,0 +1,35 @@
+from types import List
+
+
+class Solution1:
+    """解法一: 左闭右开
+    """
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return right + 1
+
+
+class Solution2:
+    """解法二: 左闭右闭
+    """
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+
+        while left < right:
+            middle = left + (right - left) // 2
+            if nums[middle] > target:
+                right = middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return right
