cf2fee50ce6e426aee321614c997e7cefa91560f
lzzzzl <597023590@qq.com>
2023-07-11 07:58:10 +0800

    solution 1

--- array/3-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ array/3-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,7 +1,7 @@
 from types import List
 
 class Solution1:
-    """
+    """解法一: 左闭右闭
     """
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         def getRightBorder(nums: List[int], target: int) -> int:
@@ -17,3 +17,29 @@ class Solution1:
 
             return rightBorder
         
+        def getLeftBorder(nums: List[int], target: int) -> int:
+            left, right = 0, len(nums) - 1
+            leftBorder = -2
+            while left <= right:
+                middle = left + (right - left) // 2
+                if nums[middle] >= target:
+                    right = middle - 1
+                    leftBorder = right
+                else:
+                    left = middle + 1
+            return leftBorder
+        
+        leftBorder = getLeftBorder(nums, target)
+        rightBorder = getRightBorder(nums, target)
+        # 情况一: target 在数组范围的右边或者左边，例如数组{3, 4, 5}，
+        # target 为 2 或者数组 {3, 4, 5}, target 为 6，此时应该返回 
+        # {-1, -1}
+        if leftBorder == -2 or rightBorder == -2:
+            return [-1, -1]
+        # 情况三: target 在数组范围中，且数组中存在target，
+        # 例如数组{3,6,7},target为6，此时应该返回{1, 1}
+        if rightBorder - leftBorder > 1:
+            return [leftBorder + 1, rightBorder - 1]
+        # 情况二: target 在数组范围中，且数组中不存在target，
+        # 例如数组{3,6,7},target为5，此时应该返回{-1, -1}
+        return [-1, -1]
