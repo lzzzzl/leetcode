9ae00ea0db356cc25ab8ef485625a335f7621854
lzzzzl <597023590@qq.com>
2023-07-13 07:47:57 +0800

    sqrt solution 1

--- array/4-sqrtx/sqrtx.py
+++ array/4-sqrtx/sqrtx.py
@@ -1,3 +1,15 @@
-class Solution:
+class Solution1:
+    """解法一: 二分查找法【左闭右闭】
+    """
     def mySqrt(self, x: int) -> int:
-        return
+        if x <= 1:
+            return x
+        left, right = 0, x
+        while left <= right:
+            middle = left + (right - left) // 2
+            if middle * middle <= x < (middle + 1) * (middle + 1):
+                return middle
+            elif x < middle * middle:
+                right = middle - 1
+            else:
+                left = middle + 1
