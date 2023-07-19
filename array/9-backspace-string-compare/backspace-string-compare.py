433314320ecd1f3cb947ffc620616cda1af443a3
lzzzzl <597023590@qq.com>
2023-07-19 08:01:04 +0800

    backspace string compare solution 2

--- array/9-backspace-string-compare/backspace-string-compare.py
+++ array/9-backspace-string-compare/backspace-string-compare.py
@@ -1,6 +1,7 @@
 class Solution1:
     """解法一: 堆栈
     """
+
     def backspaceCompare(self, s: str, t: str) -> bool:
         def process_string(str):
             stack = []
@@ -12,3 +13,30 @@ class Solution1:
             return ''.join(stack)
 
         return process_string(s) == process_string(t)
+
+
+class Solution2:
+    """解法二: 快慢指针
+    """
+
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s, t = list(s), list(t)
+        s_slow = t_slow = 0
+        
+        for fast, x in enumerate(s):
+            if x == '#':
+                if s_slow != 0:
+                    s_slow -= 1
+                continue
+            if s_slow != fast: s[s_slow] = s[fast]
+            s_slow += 1
+        
+        for fast, x in enumerate(t):
+            if x == '#':
+                if t_slow != 0:
+                    t_slow -= 1
+                continue
+            if t_slow != fast: t[t_slow] = t[fast]
+            t_slow += 1
+
+        return s[:s_slow] == t[:t_slow]
