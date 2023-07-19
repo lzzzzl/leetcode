708b8fd6d50590b688395d9349e18db437635a80
lzzzzl <597023590@qq.com>
2023-07-20 07:55:33 +0800

    binary search golang version

---
+++ array/1-binary-search/binary_search.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type Solution1 struct{}
+
+func (s Solution1) search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		middle := (left + right) / 2
+		if nums[middle] < target {
+			left = middle + 1
+		} else if nums[middle] > target {
+			right = middle - 1
+		} else {
+			return middle
+		}
+	}
+
+	return -1
+}
+
+type Solution2 struct{}
+
+func (s Solution2) search(nums []int, target int) int {
+	left, right := 0, len(nums)
+
+	for left < right {
+		middle := (left + right) / 2
+		if nums[middle] < target {
+			left = middle + 1
+		} else if nums[middle] > target {
+			right = middle
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	s1 := Solution1{}
+	sr := s1.search([]int{-1, 0, 3, 5, 9, 12}, 9)
+	fmt.Println(sr)
+
+	s2 := Solution2{}
+	sc := s2.search([]int{-1, 0, 3, 5, 9, 12}, 9)
+	fmt.Println(sc)
+}
